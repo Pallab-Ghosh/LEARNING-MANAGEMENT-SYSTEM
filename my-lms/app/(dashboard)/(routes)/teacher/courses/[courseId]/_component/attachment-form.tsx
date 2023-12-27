70679f12c8cadf57c55e81bd7c2539247f3364f7
PALLAB GHOSH <gpallab405@gmail.com>
2023-12-27 19:32:21 +0530

    Update the component
    1.create onDelete for deleting the attachments.
    2.Add UI for deleting the attachments.

--- my-lms/app/(dashboard)/(routes)/teacher/courses/[courseId]/_component/attachment-form.tsx
+++ my-lms/app/(dashboard)/(routes)/teacher/courses/[courseId]/_component/attachment-form.tsx
@@ -8,7 +8,7 @@ import {useForm} from 'react-hook-form'
 import {Form,FormControl,FormItem,FormField,FormMessage, FormLabel, FormDescription} from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
-import { ImageIcon, Pencil, PlusCircle } from 'lucide-react'
+import { File, ImageIcon, Loader, Loader2, Pencil, PlusCircle, X } from 'lucide-react'
 import { useEffect, useState } from 'react'
 import toast from 'react-hot-toast'
 import { useRouter } from 'next/navigation'
@@ -34,15 +34,15 @@ const formSchema=z.object({
 const AttachmentForm = ({initialData,courseId}:AttachmentFormProps) => {
 
 const[isEditing,setEditting]=useState(false);
-
-
+const[deletingId,setdeletingId]=useState<string | null>(null);
 const router=useRouter()
+
 
 const onSubmit=async(values:z.infer<typeof formSchema>)=>{
        //alert(values.imageUrl)
       try {
        const fetchdata= await axios.post(`/api/course/${courseId}/attachments`,values)
-       console.log(fetchdata.data)
+      // console.log(fetchdata.data)
         toast.success('Course Updated')
         toggleEdit();
         window.location.reload();
@@ -52,6 +52,25 @@ const onSubmit=async(values:z.infer<typeof formSchema>)=>{
         }
 }
 
+
+//delete the attachments
+
+const onDelete=async(id:string)=>{
+   try
+    {
+    setdeletingId(id);
+    console.log('deletingId',deletingId)
+    await axios.delete(`/api/course/${courseId}/attachments/${id}`);
+    toast.success('Attachment deleted');
+    window.location.reload();
+    } 
+   catch (error) {
+     toast.error('Something went wrong')
+   }
+   finally{
+    setdeletingId(null)
+   }
+}
 
 
 const toggleEdit=()=>{
@@ -93,6 +112,39 @@ const toggleEdit=()=>{
                 initialData.attachments.length=== 0 && (
                   <p className='text-sm mt-2 text-slate-500 italic'> No attachments yet</p>
                 ) }
+
+                {
+                   initialData.attachments.length > 0 && (
+                    <div className='space-y-2'>
+                      {
+                        initialData.attachments.map((attachment_file)=>(
+                          <div key={attachment_file.id} className=' flex items-center p-3 w-full bg-sky-100 border-sky-200 text-sky-700 rounded-md'>
+                             <File className='h-4 w-4 mr-2 flex-shrink-0'/>
+                             <p className='text-xs line-clamp-1'>
+                              {attachment_file.name}
+                              </p>
+                               
+                               {
+                                deletingId===attachment_file.id && (
+                                  <div>
+                                      <Loader2 className='h-5 w-5 animate-spin'/>
+                                  </div>
+                                )
+                               }
+
+                               {
+                                 deletingId!==attachment_file.id  && (
+                                  <Button variant='destructive' className='ml-auto hover:opacity-75 transition' onClick={()=>onDelete(attachment_file.id)}>
+                                      <X className='h-4 w-4 '/>
+                                  </Button>
+                               )}
+                          </div>
+                        ))
+                      }
+
+                    </div>
+                   )
+                }
                 </>
               )}
 
