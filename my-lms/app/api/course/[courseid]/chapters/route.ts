c7213116165eef15f1df2a9982a7d03610cd0f7b
PALLAB GHOSH <gpallab405@gmail.com>
2023-12-31 21:28:13 +0530

    create route.ts
    1.create the api to create the chapter in database

---
+++ my-lms/app/api/course/[courseid]/chapters/route.ts
@@ -0,0 +1,65 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+import { type } from "os";
+
+type params_type={
+    params:{
+        courseid:string
+    }
+}
+
+export async function POST(req:Request,{params}:params_type)
+ {
+    try 
+    {
+        const {userId}=auth()
+        const {title}=await req.json()
+
+        if(!userId)
+        {
+            return new NextResponse('Unauthorized',{status:401})
+        }
+
+        const courseOwner=await db.course.findUnique({
+            where:{
+                id:params.courseid,
+                userId:userId
+            }
+        })
+
+        if(!courseOwner)return new NextResponse('Unauthorized',{status:401})
+        
+        const lastChapter=await db.chapter.findFirst({
+            where:{
+                courseId:params.courseid
+            },
+            orderBy:{
+                position:'desc'
+            }
+        })
+
+    const newPosition=lastChapter ? lastChapter.position+1 : 1;
+
+    const chapter=await db.chapter.create({
+        data:{
+            title,
+            courseId:params.courseid,
+            position:newPosition
+        }
+    })
+  
+      return NextResponse.json(chapter)
+     
+    }
+
+
+    catch (error)
+    {
+        console.log('[Chapter]',error)
+        return new NextResponse('Internal Error',{status:500})
+
+    }
+
+
+ }
