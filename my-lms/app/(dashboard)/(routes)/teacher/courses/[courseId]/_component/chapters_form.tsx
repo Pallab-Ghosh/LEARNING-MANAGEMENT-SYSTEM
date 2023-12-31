9a8fbeb6bbd597ef775d8357b10ddff6b9a8d19d
PALLAB GHOSH <gpallab405@gmail.com>
2023-12-31 21:30:21 +0530

    update component
    1.update Textarea ->Input
    2.add button of create chapters
    3.add list of chapters

--- my-lms/app/(dashboard)/(routes)/teacher/courses/[courseId]/_component/chapters_form.tsx
+++ my-lms/app/(dashboard)/(routes)/teacher/courses/[courseId]/_component/chapters_form.tsx
@@ -8,30 +8,32 @@ import {useForm} from 'react-hook-form'
 import {Form,FormControl,FormItem,FormField,FormMessage, FormLabel, FormDescription} from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
-import { Pencil } from 'lucide-react'
+import { Pencil, Plus, PlusCircle } from 'lucide-react'
 import { useEffect, useState } from 'react'
 import toast from 'react-hot-toast'
 import { useRouter } from 'next/navigation'
 import { cn } from '../../../../../../../lib/utils';
 import { Textarea } from '@/components/ui/textarea'
-import { Course } from '@prisma/client'
+import { Course ,Chapter} from '@prisma/client'
+import ChapterList from './chapters-list'
+
+
  
 
-
 type ChapterFormProps={
-    initialData:Course
+    initialData:Course & {chapters:Chapter[]}
     courseId:string
 }
 
 
 const formSchema=z.object({
-    description:z.string().min(1,{message:'Description is required'})
+    title:z.string().min(1)
 })
 
 
 const ChapterForm = ({initialData,courseId}:ChapterFormProps) => {
-
-const[isEditing,setEditting]=useState(false);
+const [isCreating,setIsCreating]=useState(false)
+const[isUpdating,setUpDating]=useState(false);
 
 const router=useRouter()
 
@@ -40,7 +42,7 @@ const router=useRouter()
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues:{
-      description:initialData.description || ""
+      title : ""
     }
   })
 
@@ -53,8 +55,9 @@ const onSubmit=async(values:z.infer<typeof formSchema>)=>{
  // console.log(values)
 
       try {
-        await axios.patch(`/api/course/${courseId}`,values)
-        toast.success('Course Updated')
+        await axios.post(`/api/course/${courseId}/chapters`,values)
+        toast.success('Chapter Updated')
+        toggleCreating()
         window.location.reload();
       }
 
@@ -65,47 +68,70 @@ const onSubmit=async(values:z.infer<typeof formSchema>)=>{
 
 
 
-const toggleEdit=()=>{
-  setEditting((current)=>!current)
+const toggleCreating=()=>{
+  setIsCreating((current)=>!current)
 }
 
   return (
     <div className='mt-6 border bg-slate-100 rounded-md p-4'>
         <div className='font-medium flex items-center justify-between'>
-          Course Description
-          <Button onClick={toggleEdit}>
+          Course Chapter
+          <Button onClick={toggleCreating}>
             {
-          isEditing ? ( <>Cancel</>):  (  <> <Pencil className='h-4 w-4 mr-2'/>Edit Description</>)
+          isCreating? ( <>Cancel</>):  (  <> <PlusCircle className='h-4 w-4 mr-2'/>Add a Chapter</>)
             }
             
           </Button>
         </div>
       {
-        !isEditing ? (
-        <p className={cn('text-sm mt-2',  !initialData.description && 'text-slate-500 italic')}> {initialData.description  || 'No description'} </p>
-        ):
+        isCreating &&
         (
           <Form {...form}>
                    <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-4 mt-4'>
                       <FormField
                         control={form.control}
-                        name='description'
+                        name='title'
                         render={({field})=>(
                         <FormItem>
-                          <FormLabel>Course Description</FormLabel>
+                          <FormLabel>Course Chapter</FormLabel>
                            <FormControl>
-                            <Textarea disabled={isSubmitting} placeholder="e.g. 'This course about...'"  {...field}/>
+                            <Input disabled={isSubmitting} placeholder="e.g. 'Introduction to the course...'"  {...field}/>
                            </FormControl>
                            <FormMessage/>
                         </FormItem>
                       )}
                       />
 
-                      <div className='flex items-center gap-x-2'>
-                      <Button type='submit' disabled={!isValid || isSubmitting}> Save </Button>
-                      </div>
+                       
+                      <Button type='submit' disabled={!isValid || isSubmitting}> Create </Button>
+                     
                    </form>
               </Form>
+        )
+      }
+
+      {
+        !isCreating && (
+          <div className={cn(
+            'text-sm mt-2',
+            !initialData.chapters.length && 'text-slate-500 italic'
+
+          )}>
+            { !initialData.chapters.length && 'No Chapters'}
+            <ChapterList
+            onEdit={()=>{}}
+            onReorder={()=>{}}
+            items={initialData.chapters || []}
+            />
+          </div>
+        )
+      }
+
+      {
+        !isCreating && (
+          <p className=' text-xs text-muted-foreground mt-4'>
+            Drag and drop to reorder the Chapters
+          </p>
         )
       }
       </div>
