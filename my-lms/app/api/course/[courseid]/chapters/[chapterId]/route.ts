1b44aacd4683d6f9983f6cfa4246320a73c5d010
PALLAB GHOSH <gpallab405@gmail.com>
2024-01-05 00:57:08 +0530

    update api
    1.create Mux
    2.check any video exist if you user try to update video then first deleted it and then created new video data in mux and also in db

--- my-lms/app/api/course/[courseid]/chapters/[chapterId]/route.ts
+++ my-lms/app/api/course/[courseid]/chapters/[chapterId]/route.ts
@@ -2,6 +2,14 @@ import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 import { type } from "os";
+import Mux from '@mux/mux-node'
+
+
+const {Video}=new Mux(
+    process.env.MUX_TOKEN_ID!,
+    process.env.MUX_TOKEN_SECRET!
+);
+
 
 
 type params_props={
@@ -39,10 +47,43 @@ export async function PATCH(req:Request,{params}:params_props){
          data:{...values}
       })
 
+      if(values.videoUrl)
+      {
+        const existingMuxData=await db.muxData.findFirst({
+            where:{
+                chapterId:params.chapterId
+            }
+        })
+
+        if(existingMuxData)
+        {
+           await Video.Assets.del(existingMuxData.assetId);
+           await db.muxData.delete({
+            where:{
+                id:existingMuxData.id
+            }
+           })
+        }
+
+        const asset=await Video.Assets.create({
+            input:values.videoUrl,
+            playback_policy:'public',
+            test:false
+        })
+
+        await db.muxData.create({
+           data:{
+            chapterId:params.chapterId,
+            assetId:asset.id,
+            playbackId:asset.playback_ids?.[0]?.id,
+           }
+        })
+      }
+
       
+
       return NextResponse.json(chapter)
     } 
-
 
     catch (error) {
         
