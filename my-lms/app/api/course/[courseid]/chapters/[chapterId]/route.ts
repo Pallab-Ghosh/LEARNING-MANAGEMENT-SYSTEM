dc50e8898fbeb214a2efe03144f10dfbadf74c7b
PALLAB GHOSH <gpallab405@gmail.com>
2024-01-10 19:26:21 +0530

    update route
    1.create DELETE method for deleting the chapter
    
    2.check existing mux data and delete it.
    
    3.check that chapter is only in course then delete it and again make it unpublish

--- my-lms/app/api/course/[courseid]/chapters/[chapterId]/route.ts
+++ my-lms/app/api/course/[courseid]/chapters/[chapterId]/route.ts
@@ -5,12 +5,6 @@ import { type } from "os";
 import Mux from '@mux/mux-node'
 
 
-const muxTokenId = process.env.MUX_TOKEN_ID;
-const muxTokenSecret = process.env.MUX_TOKEN_SECRET;
-
-
-
-
 
 type params_props={
     params:{
@@ -19,31 +13,111 @@ type params_props={
     }
 }
 
+
+const muxTokenId = process.env.MUX_TOKEN_ID;
+const muxTokenSecret = process.env.MUX_TOKEN_SECRET;
+
+const {Video}=new Mux( muxTokenId!,muxTokenSecret!);
+
+
+
+export async function DELETE(req:Request,{params}:params_props)
+{
+
+    try 
+    {
+
+        const {userId}=auth();
+      
+    if(!userId) return new NextResponse('Unauthorized',{status:403});
+
+    const courseOwner=await db.course.findUnique({
+        where:{ id:params.courseid,  userId:userId}
+    })
+
+    if(!courseOwner)return new NextResponse('Unauthorized',{status:403})
+
+      const chapter=await db.chapter.findUnique({
+         where: { id:params.chapterId, courseId:params.courseid},
+      })
+
+      if(!chapter)
+      {
+        return new NextResponse('Not Found',{status:404})
+      }
+        
+      if(chapter.videoUrl)
+      {
+            const existingMuxdata=await db.muxData.findFirst({
+                where:{
+                    chapterId:params.chapterId
+                }
+            })
+
+            if(existingMuxdata)
+            {
+                await Video.Assets.del(existingMuxdata.assetId)
+                await db.muxData.delete({
+                    where:{
+                        id:existingMuxdata.id
+                    }
+                })
+            }
+      }
+       
+      const deleted_chapter=await db.chapter.delete({
+        where:{
+            id:params.chapterId
+        }
+      })
+
+      const publishedChapterInCourse=await db.chapter.findMany({
+        where:{
+            courseId:params.courseid,
+            isPublished:true,
+        }
+      })
+
+      if(!publishedChapterInCourse.length)
+      {
+          await db.course.update({
+            where:{
+                id:params.courseid
+            },
+            data:{
+                isPublished:false
+            }
+          })
+      }
+     return NextResponse.json(deleted_chapter);
+    } 
+
+    catch (error) {
+        console.log('[Chapter delete Error]',error)
+        return new NextResponse('Internal Error',{status:500})
+    }
+}
+
+
+
+
 export async function PATCH(req:Request,{params}:params_props)
 {
 
     try 
     {
 
-        if (!muxTokenId || !muxTokenSecret) {
-            return new NextResponse('Internal Server Error: Mux API credentials not provided.', { status: 500 });
-        }
-        
-        const {Video}=new Mux( muxTokenId!,muxTokenSecret!);
+     
         
         const {userId}=auth();
         const {isPublished , ...values}=await req.json();
-         
+         console.log(values)
 
 
-    if(!userId)
-    return new NextResponse('Unauthorized',{status:403});
+    if(!userId) return new NextResponse('Unauthorized',{status:403});
 
     const courseOwner=await db.course.findUnique({
-        where:{
-            id:params.courseid,
-            userId:userId
-        }
+        where:{ id:params.courseid,  userId:userId}
     })
 
     if(!courseOwner)return new NextResponse('Unauthorized',{status:403})
@@ -55,38 +129,39 @@ export async function PATCH(req:Request,{params}:params_props)
 
       if(values.videoUrl)
       {
-        const existingMuxData=await db.muxData.findFirst({
-             where:{ chapterId:params.chapterId }
-        })
+                const existingMuxData=await db.muxData.findFirst({
+                    where:{ chapterId:params.chapterId }
+                })
  
         
 
-        if(existingMuxData)
-        {
-           await Video.Assets.del(existingMuxData.assetId);
-           await db.muxData.delete({
-            where:{id:existingMuxData.id}
-           })
-        }
+                if(existingMuxData)
+                {
+                    await Video.Assets.del(existingMuxData.assetId);
+                    await db.muxData.delete({
+                        where:{id:existingMuxData.id}
+                    })
+                }
 
-        const asset=await Video.Assets.create({
-            input:values.videoUrl,
-            playback_policy:'public',
-            test:false
-        })
-
-       
- 
-       const video= await db.muxData.create({
-           data:{
-            chapterId:params.chapterId,
-            assetId:asset.id,
-            playbackId:asset.playback_ids?.[0]?.id,
-           }
-        })
-        return  NextResponse.json(video)
+                const asset=await Video.Assets.create({
+                    input:values.videoUrl,
+                    playback_policy:'public',
+                    test:false
+                })
+               
+                console.log('asset',asset)
+            
+        
+            await db.muxData.create({
+                data:{
+                    chapterId:params.chapterId,
+                    assetId:asset.id,
+                    playbackId:asset.playback_ids?.[0]?.id,
+                }
+                })
+                
       }
-
+      return  NextResponse.json(chapter)
      
     } 
 
