1e3a1700f57b8aab4ccd48536a417de15a29d187
PALLAB GHOSH <gpallab405@gmail.com>
2024-01-11 19:13:14 +0530

    change logic that 1 chapter left and if it is unpublished then the course should be unpublished

--- my-lms/app/api/course/[courseid]/chapters/[chapterId]/unpublish/route.ts
+++ my-lms/app/api/course/[courseid]/chapters/[chapterId]/unpublish/route.ts
@@ -46,7 +46,7 @@ export async function PATCH(req:Request,{params}:params_props) {
 
       
 
-      if(publishedchapterincourse.length)
+      if(!publishedchapterincourse.length)
       {
          await db.course.update({
             where:{
