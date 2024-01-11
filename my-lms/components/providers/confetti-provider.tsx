beb67d7a8d165896121dd9f07f45897bdfc44dfb
PALLAB GHOSH <gpallab405@gmail.com>
2024-01-11 20:04:56 +0530

    create ConfettiProvider for providing everwhere in app

---
+++ my-lms/components/providers/confetti-provider.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import { useConfettiStore } from "@/hooks/use-confetti-store"
+import ReactConfetti from "react-confetti"
+
+export const ConfettiProvider=()=>{
+
+    const {isOpen,onClose,onOpen}=useConfettiStore();
+
+    if(!isOpen)return null;
+
+      return(
+        <ReactConfetti
+         className='pointer-events-none z-[100]'
+         numberOfPieces={500}
+         recycle={false}
+         onConfettiComplete={()=>{
+            onClose();
+         }}
+        />
+      )
+}
