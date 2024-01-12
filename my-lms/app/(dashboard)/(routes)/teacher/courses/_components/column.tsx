9c2bcb4619c5ad4fd3c169187aad9aded7cf7c1f
PALLAB GHOSH <gpallab405@gmail.com>
2024-01-12 23:45:28 +0530

    update the component

--- my-lms/app/(dashboard)/(routes)/teacher/courses/_components/column.tsx
+++ my-lms/app/(dashboard)/(routes)/teacher/courses/_components/column.tsx
@@ -1,22 +1,97 @@
 "use client"
 
+import { Button } from "@/components/ui/button"
 import { Course } from "@prisma/client"
 import { ColumnDef } from "@tanstack/react-table"
-
-
+import { ArrowUpDown, Home, MoreHorizontal, Pencil } from "lucide-react"
+import {
+  DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger} from "@/components/ui/dropdown-menu"
+import Link from "next/link"
 
 
 export const columns: ColumnDef<Course>[] = [
   {
     accessorKey: "title",
-    header: "Title",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Title
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    },
   },
+
   {
     accessorKey: "price",
-    header: "Price",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Price
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    },
   },
+
   {
     accessorKey: "isPublished",
-    header: "Published",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Published
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    },
   },
+
+    {
+      accessorKey: "Actions",
+      id:"actions",
+      cell: ({ row }) => {
+         const {id} =row.original
+         return(
+          <DropdownMenu>
+
+              <DropdownMenuTrigger asChild>
+
+                  <Button className=" h-4 w-8 p-0">
+                      <span className="sr-only">Open Menu</span>
+                      <MoreHorizontal className=" h-4 w-4"/>
+                  </Button>
+
+              </DropdownMenuTrigger>  
+
+              <DropdownMenuContent align="end">
+                  <Link href={`/teacher/courses/${id}`}>
+                     <DropdownMenuItem>
+                        <Pencil className=" h-4 w-4 mr-2"/>
+                        Edit
+                     </DropdownMenuItem>
+                  </Link>
+
+                  <Link href={`/`}>
+                     <DropdownMenuItem>
+                        <Home className=" h-4 w-4 mr-2"/>
+                         Home
+                     </DropdownMenuItem>
+                  </Link>
+
+              </DropdownMenuContent>
+          </DropdownMenu>
+         )
+      }
+      
+    }
+
 ]
