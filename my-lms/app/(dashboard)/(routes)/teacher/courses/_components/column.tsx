ce8c6958451efeed6560d0b0e99b9ceb4e0d548f
PALLAB GHOSH <gpallab405@gmail.com>
2024-01-13 00:38:30 +0530

    get the value of isPublished and based on that update the UI

--- my-lms/app/(dashboard)/(routes)/teacher/courses/_components/column.tsx
+++ my-lms/app/(dashboard)/(routes)/teacher/courses/_components/column.tsx
@@ -4,9 +4,10 @@ import { Button } from "@/components/ui/button"
 import { Course } from "@prisma/client"
 import { ColumnDef } from "@tanstack/react-table"
 import { ArrowUpDown, Home, MoreHorizontal, Pencil } from "lucide-react"
-import {
-  DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger} from "@/components/ui/dropdown-menu"
+import {  DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger} from "@/components/ui/dropdown-menu"
 import Link from "next/link"
+import { Badge } from "@/components/ui/badge"
+import { cn } from "@/lib/utils"
 
 
 export const columns: ColumnDef<Course>[] = [
@@ -14,10 +15,7 @@ export const columns: ColumnDef<Course>[] = [
     accessorKey: "title",
     header: ({ column }) => {
       return (
-        <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
+        <Button  variant="ghost"  onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}  >
           Title
           <ArrowUpDown className="ml-2 h-4 w-4" />
         </Button>
@@ -29,10 +27,7 @@ export const columns: ColumnDef<Course>[] = [
     accessorKey: "price",
     header: ({ column }) => {
       return (
-        <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
+        <Button  variant="ghost"  onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
           Price
           <ArrowUpDown className="ml-2 h-4 w-4" />
         </Button>
@@ -44,15 +39,26 @@ export const columns: ColumnDef<Course>[] = [
     accessorKey: "isPublished",
     header: ({ column }) => {
       return (
-        <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
+        <Button  variant="ghost"  onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
           Published
           <ArrowUpDown className="ml-2 h-4 w-4" />
         </Button>
       )
     },
+
+    cell:({row})=>{
+       const isPublished=row.getValue('isPublished') || false
+      // const { isPublished } = row.original || false; same as line no 50 code
+       return (
+        <div>
+           <Badge className={cn("bg-slate-500", isPublished && 'bg-sky-700')}>
+            {
+              isPublished ? 'Published' : 'Draft'
+            }
+           </Badge>
+        </div>
+       )
+    }
   },
 
     {
