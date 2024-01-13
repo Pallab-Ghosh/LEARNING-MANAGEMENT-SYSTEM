5750357ccb06cdcdeec14437ef29d699719866df
PALLAB GHOSH <gpallab405@gmail.com>
2024-01-13 19:11:23 +0530

    get the url params and modify it to set the params with course categoryId .

--- my-lms/app/(dashboard)/(routes)/search/_components/CategoryItem.tsx
+++ my-lms/app/(dashboard)/(routes)/search/_components/CategoryItem.tsx
@@ -1,5 +1,9 @@
 
 import { Button } from '@/components/ui/button'
+import { cn } from '@/lib/utils'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+ 
+import qs from 'query-string'
 
 import React from 'react'
 import { IconType } from 'react-icons/lib'
@@ -14,12 +18,38 @@ type CategoryItemprops={
 
 const CategoryItem = ({key,label,icon:Icon,value}:CategoryItemprops) => {
 
+      const pathname=usePathname()
+      const router=useRouter()
+      const searchParams=useSearchParams();
+
+      const currentCategoryId=searchParams.get('categoryId');
+      const currentTitle=searchParams.get('title')
+      const isSelected=currentCategoryId===value
+      
+
+      const onClick=()=>{
+          const url=qs.stringifyUrl({
+            url:pathname,
+            query:{
+                title:currentTitle,
+                categoryId:isSelected ? null : value
+            }
+          },{skipNull:true , skipEmptyString: true})
+
+          router.push(url)
+      }
+
   return (
     <div>
-        <Button>
-            {
-            Icon && <Icon size={20} />
-            }
+        <Button className={
+            cn('py-2 px-3 text-sm border border-slate-200 first-letter rounded-full flex items-center gap-x-1 hover:border-sky-700 transition', 
+             isSelected && ' border-sky-700 bg-sky-200/20 text-sky-800'
+            )}
+        type='button'
+        variant='ghost'
+        onClick={onClick}
+        >
+            { Icon && <Icon size={20} /> }
             <div className=' truncate'>
                 {label}
             </div>
