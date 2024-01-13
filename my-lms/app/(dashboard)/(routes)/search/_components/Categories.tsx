2c14e0ac9d7d5c585aabffaee0c693fa8ea63f85
PALLAB GHOSH <gpallab405@gmail.com>
2024-01-13 12:45:17 +0530

    send the key label icon value as a props to CategoryItem

--- my-lms/app/(dashboard)/(routes)/search/_components/Categories.tsx
+++ my-lms/app/(dashboard)/(routes)/search/_components/Categories.tsx
@@ -1,15 +1,40 @@
 'use client'
 
 import { Category } from "@prisma/client"
+
 import { type } from "os"
+import {FcEditImage, FcEngineering,FcFilmReel,FcMultipleDevices,FcMusic,FcOldTimeCamera,FcSalesPerformance,FcSportsMode} from 'react-icons/fc'
+import { IconType } from "react-icons/lib"
+import CategoryItem from "./CategoryItem"
 
 type categories_props={
-    items:Category[];
+    items:Category[],
+}
+
+const iconMap: Record<Category['name'], IconType>={
+    'Music':FcMusic,
+    'Photography':FcOldTimeCamera,
+    'Computer Science':FcMultipleDevices,
+    'Fitness':FcSportsMode,
+    'Accounting':FcSalesPerformance,
+    'Engineering':FcEditImage,
+    'Filming':FcFilmReel
 }
 
 const Categories = ({items}:categories_props) => {
   return (
-    <div>Categories</div>
+    <div className=" flex items-center gap-x-2 overflow-x-auto pb-2">
+      {
+        items.map((item)=>(
+         <CategoryItem
+         key={item.id}
+         label={item.name}
+         icon={iconMap[item.name]}
+         value={item.id}
+         />
+        ))
+      }
+    </div>
   )
 }
 
