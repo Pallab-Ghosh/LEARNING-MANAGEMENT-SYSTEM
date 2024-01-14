0d47aa15de2f43f2947070ff1a9413ac1a8e620e
PALLAB GHOSH <gpallab405@gmail.com>
2024-01-14 14:05:00 +0530

    add   <SearchInput/> component and change the UI based on search page or not

--- my-lms/components/navbar-routes.tsx
+++ my-lms/components/navbar-routes.tsx
@@ -7,6 +7,7 @@ import React from 'react'
 import { Button } from './ui/button'
 import {  LogOut } from 'lucide-react'
 import Link from 'next/link'
+import { SearchInput } from './search-input'
 
 const NavbarRoutes = () => {
 
@@ -15,14 +16,23 @@ const NavbarRoutes = () => {
 
 const isTeacherPage=pathname?.startsWith('/teacher');
 const isPlayerPage=pathname?.startsWith('/chapter');
+const isSeacrhPage=pathname==='/search'
 
   return (
+    <>
+     {
+       isSeacrhPage && (
+        <div className=' hidden md:block'>
+           <SearchInput/>
+        </div>
+       )
+     }
     <div className='flex gap-x-2 ml-auto'>
         {
             isTeacherPage || isPlayerPage ?
             (
               <Link href='/'>
-                <Button size='sm' variant='ghost'>
+                <Button size='sm'>
                     <LogOut className='h4 w-4 mr-2' />
                     Exit
                 </Button>
@@ -30,7 +40,7 @@ const isPlayerPage=pathname?.startsWith('/chapter');
             ):
             (
                 <Link href='/teacher/courses'>
-                  <Button size='sm' variant='ghost'>
+                  <Button size='sm' >
                     Teacher Mode
                   </Button>
                 </Link>
@@ -40,6 +50,7 @@ const isPlayerPage=pathname?.startsWith('/chapter');
         afterSignOutUrl='/'
         />
     </div>
+    </>
   )
 }
 
