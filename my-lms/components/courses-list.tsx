e3010bcf9623b88097d5d96811f83d2bb3b365d1
PALLAB GHOSH <gpallab405@gmail.com>
2024-01-16 19:00:10 +0530

    get the course send the course to  CourseCard component for showing in UI

--- my-lms/components/courses-list.tsx
+++ my-lms/components/courses-list.tsx
@@ -1,4 +1,5 @@
 import { CourseWithProgressWithCategory } from "@/action/get-courses"
+import CourseCard from "./course-card"
 
  
 type CoursesListProps ={
@@ -6,15 +7,32 @@ type CoursesListProps ={
 }
 const CoursesList = ({items}:CoursesListProps) => {
   return (
-    <div>
+   <div>
+            <div className=" grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-4">
+            {
+                items.map((item)=>(
+                   <CourseCard
+                   key={item.id}
+                   id={item.id}
+                   title={item.title}
+                   imageUrl={item.imageUrl!}
+                   chaptersLength={item.chapters.length}
+                   price={item.price!}
+                   progress={item.progress}
+                   category={item?.category?.name}
+                   />
+                ))
+            }
+        </div>
+      
+      <div className=" text-center text-sm text-muted-foreground mt-10">
         {
-            items.map((item)=>(
-                <div key={item.id}>
-                    <div>{item.title}</div>
-                </div>
-            ))
+            items.length ===0 && (
+                <div>No Courses Found</div>
+            )
         }
-    </div>
+      </div>
+   </div>
   )
 }
 
