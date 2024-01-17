d25c7eba9cfbe0744742908417da0575d474c6b5
PALLAB GHOSH <gpallab405@gmail.com>
2024-01-18 01:25:59 +0530

    fetch the course and progressCount and send them as a props

--- my-lms/app/(course)/course/[courseId]/layout.tsx
+++ my-lms/app/(course)/course/[courseId]/layout.tsx
@@ -1,5 +1,8 @@
+import { getProgress } from "@/action/get-progress";
+import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs"
 import { redirect } from "next/navigation"
+import CourseSidebar from "./_components/course-sidebar";
 
  
  
@@ -13,11 +16,40 @@ import { redirect } from "next/navigation"
    if(!userId)
    return redirect('/');
 
-   
+   const course=await db.course.findUnique({
+    where:{id:params.courseId},
+    include:{
+        chapters:{ 
+            where:{isPublished:true},
+            include:{
+                userProgress:{
+                     where:{userId}  
+                    }
+            },
+            orderBy:{
+                position:'asc'
+            }
+         } 
+    }
+   })
+
+   if(!course)
+   return redirect('/');
+
+   const progressCount=await getProgress({ userId, courseId: course.id })
+
 
     return (
-     <div>
+     <div className=" h-full">
+        <div className=" hidden md:flex h-full w-80 flex-col fixed inset-y-0 z-50">
+            <CourseSidebar
+             course={course}
+             progressCount={progressCount}
+            />
+        </div>
+        <main className=" md:pl-80 h-full">
         {children}
+        </main>
      </div>
   )
 }
