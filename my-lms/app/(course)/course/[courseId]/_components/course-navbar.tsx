4867312e7885309d14e6ba4ebdd7f73c18f97473
PALLAB GHOSH <gpallab405@gmail.com>
2024-01-18 23:46:39 +0530

    Add <CourseMobileSidebar/> component

--- my-lms/app/(course)/course/[courseId]/_components/course-navbar.tsx
+++ my-lms/app/(course)/course/[courseId]/_components/course-navbar.tsx
@@ -1,5 +1,6 @@
 import NavbarRoutes from "@/components/navbar-routes"
 import { Chapter, Course, UserProgress } from "@prisma/client"
+import CourseMobileSidebar from "./course-mobile-sidebar"
 
 
 type CourseNavbarProps={
@@ -14,6 +15,10 @@ type CourseNavbarProps={
 const CourseNavbar = ({course,progressCount}:CourseNavbarProps) => {
   return (
     <div className= " mr-4 border-b h-full flex items-center bg-white shadow-sm">
+         <CourseMobileSidebar
+         course={course}
+         progressCount={progressCount}
+         />
         <NavbarRoutes/>
     </div>
   )
