ffb6043cbe45a065544d9d77718b6d7637730be9
PALLAB GHOSH <gpallab405@gmail.com>
2024-01-18 20:37:23 +0530

    Add border when chapter is active

--- my-lms/app/(course)/course/[courseId]/_components/course-sidebaritem.tsx
+++ my-lms/app/(course)/course/[courseId]/_components/course-sidebaritem.tsx
@@ -33,7 +33,7 @@ const CourseSidebarItem= ({id,label,isCompleted,courseId,islocked}:CourseSidebar
       onClick={onClick}
 
     className={cn('flex items-center gap-x-2 text-slate-500 text-sm font-[500] pl-6 transition-all hover:text-slate-600 hover:bg-slate-300/20',
-    isActive && 'text-slate-700 bg-slate-200/20 hover:bg-slate-200/20 hover:text-slate-700',
+    isActive && 'text-slate-700 bg-lime-200 hover:bg-emerald-100 hover:text-slate-700',
     isCompleted && ' text-emerald-700 hover:text-emerald-700',
     isCompleted && isActive && 'bg-slate-200/20'
     )}
@@ -47,7 +47,13 @@ const CourseSidebarItem= ({id,label,isCompleted,courseId,islocked}:CourseSidebar
               />
               {label}
         </div>
-        
+        <div
+        className={cn(
+            "ml-auto opacity-0 border-2  border-orange-800 h-full transition-all",
+            isActive && 'opacity-100',
+            isCompleted && 'border-emerald-700'
+        )}
+        />
 
          
         
