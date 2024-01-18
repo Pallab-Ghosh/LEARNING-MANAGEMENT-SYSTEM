9e224424ce4dd36e92fec4202d5d63915995c57c
PALLAB GHOSH <gpallab405@gmail.com>
2024-01-18 20:44:45 +0530

    Get the courses and send them to DataTable

--- my-lms/app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ my-lms/app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -2,15 +2,35 @@ import { Button } from '@/components/ui/button'
 import Link from 'next/link'
 import React from 'react'
 
-const Coursepage = () => {
+import { DataTable } from './_components/data-table'
+import { Course } from '@prisma/client'
+import { auth } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
+import { db } from '@/lib/db'
+import { columns } from './_components/column'
+
+
+
+const Coursepage = async() => {
+
+  const {userId} = auth();
+
+  if(!userId)
+  return redirect('/');
+
+  const courses=await db.course.findMany({
+    where:{
+       userId
+    },
+    orderBy:{
+      createdAt:'asc'
+    }
+  })
+
+
   return (
     <div className='p-6'>
-      <Link href='/teacher/create'>
-        <Button>
-          New Courses
-        </Button>
-      </Link>
-      
+       <DataTable columns={columns} data={courses} />
      </div>
   )
 }
