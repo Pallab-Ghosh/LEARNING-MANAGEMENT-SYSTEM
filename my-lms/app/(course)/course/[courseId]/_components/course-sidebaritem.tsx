15c9f1120b74acc8e0a56ac6d3c144c0aa131819
PALLAB GHOSH <gpallab405@gmail.com>
2024-01-18 19:06:16 +0530

    Get pathname, router-object and Icon based on Locked and Completed ,create onClick to go for individual Chapter and edit the UI

--- my-lms/app/(course)/course/[courseId]/_components/course-sidebaritem.tsx
+++ my-lms/app/(course)/course/[courseId]/_components/course-sidebaritem.tsx
@@ -1,3 +1,9 @@
+'use client'
+import { Button } from "@/components/ui/button"
+import { cn } from "@/lib/utils"
+import { CheckCircle, Lock, PlayCircle } from "lucide-react"
+import { usePathname, useRouter } from "next/navigation"
+
  
 
 
@@ -11,8 +17,41 @@
  }
 
 const CourseSidebarItem= ({id,label,isCompleted,courseId,islocked}:CourseSidebarItemProps) => {
-  return (
-    <div>course-sidebaritem</div>
+
+    const pathname=usePathname()
+    const router=useRouter();
+
+    const Icon=islocked ? Lock : (isCompleted ? CheckCircle :PlayCircle )
+   
+    const isActive=pathname.includes(id);
+
+    const onClick=()=>{
+        router.push(`/course/${courseId}/chapters/${id}`)
+    }
+    return (
+    <button type="button" 
+      onClick={onClick}
+
+    className={cn('flex items-center gap-x-2 text-slate-500 text-sm font-[500] pl-6 transition-all hover:text-slate-600 hover:bg-slate-300/20',
+    isActive && 'text-slate-700 bg-slate-200/20 hover:bg-slate-200/20 hover:text-slate-700',
+    isCompleted && ' text-emerald-700 hover:text-emerald-700',
+    isCompleted && isActive && 'bg-slate-200/20'
+    )}
+    >
+        <div className="flex items-center gap-x-2 my-4">
+              <Icon size={22}
+              className={cn('text-slate-500',
+              isActive && 'text-slate-700',
+              isCompleted && ' text-emerald-700'
+              )}
+              />
+              {label}
+        </div>
+        
+
+         
+        
+    </button>
   )
 }
 
