42d9a9f72e3b7bb5b2863f381ec0d0f8b972654f
PALLAB GHOSH <gpallab405@gmail.com>
2024-01-18 20:37:57 +0530

    Add CourseNavbar component and send the props

--- my-lms/app/(course)/course/[courseId]/layout.tsx
+++ my-lms/app/(course)/course/[courseId]/layout.tsx
@@ -3,6 +3,7 @@ import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs"
 import { redirect } from "next/navigation"
 import CourseSidebar from "./_components/course-sidebar";
+import CourseNavbar from "./_components/course-navbar";
 
  
  
@@ -41,12 +42,22 @@ import CourseSidebar from "./_components/course-sidebar";
 
     return (
      <div className=" h-full">
+
+         <div className=" h-[80px] md:pl-80 fixed inset-y-0 w-full z-50">
+
+               <CourseNavbar
+                course={course}
+                progressCount={progressCount}
+               />
+         </div>
+
         <div className=" hidden md:flex h-full w-80 flex-col fixed inset-y-0 z-50">
             <CourseSidebar
              course={course}
              progressCount={progressCount}
             />
         </div>
+
         <main className=" md:pl-80 h-full">
         {children}
         </main>
