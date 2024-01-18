132ae568c38341e9093667b77bb679d6316ab824
PALLAB GHOSH <gpallab405@gmail.com>
2024-01-18 14:16:40 +0530

    get the purchase data based on userId and courseId, and on course which get as props, run map the data send them CourseSidebarItem component

--- my-lms/app/(course)/course/[courseId]/_components/course-sidebar.tsx
+++ my-lms/app/(course)/course/[courseId]/_components/course-sidebar.tsx
@@ -1,5 +1,10 @@
 import { Chapter, Course, UserProgress } from '@prisma/client';
 import { CourseWithProgressWithCategory } from '../../../../../action/get-courses';
+import { auth } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+import { db } from '@/lib/db';
+import CourseSidebarItem from './course-sidebaritem';
+
 
 
 
@@ -12,9 +17,44 @@ type CourseSidebarProps={
     progressCount:number
 }
 
-const CourseSidebar = ({course,progressCount}:CourseSidebarProps) => {
+const CourseSidebar = async({course,progressCount}:CourseSidebarProps) => {
+
+  const {userId}=auth()
+  if(!userId)return redirect('/');
+
+  const purchase=await db.purchase.findUnique({
+    where:{
+       userId_courseId:{
+        userId,
+        courseId:course.id
+       }
+    }
+  })
+
+
+
   return (
-    <div>course-sidebar</div>
+    <div className=' h-full border-r flex flex-col overflow-y-auto shadow-sm'>
+        <div className=' p-8 flex flex-col border-b'>
+            <h1 className=' font-semibold'>
+            {course.title}
+          </h1>
+        </div> 
+         <div className='flex flex-col w-full'>
+           {
+             course.chapters.map((chapter)=>(
+               <CourseSidebarItem
+                key={chapter.id}
+                id={chapter.id}
+                label={chapter.title}
+                isCompleted={!!chapter.userProgress?.[0]?.isCompleted}
+                courseId={course.id}
+                islocked={!chapter.isFree && !purchase}
+               />
+             ))
+           }
+         </div>
+    </div>
   )
 }
 
