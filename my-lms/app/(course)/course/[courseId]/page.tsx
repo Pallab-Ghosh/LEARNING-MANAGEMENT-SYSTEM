9bf44b5f0379a6c2c30b827c89263c4d86bd4b8d
PALLAB GHOSH <gpallab405@gmail.com>
2024-01-19 11:58:59 +0530

    Get the published course by Id and redirected to first chapter in videoPlayer page

--- my-lms/app/(course)/course/[courseId]/page.tsx
+++ my-lms/app/(course)/course/[courseId]/page.tsx
@@ -1,4 +1,6 @@
 
+import { db } from '@/lib/db'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
 type CourseIdParams={
@@ -7,7 +9,28 @@ type CourseIdParams={
     }
 }
 
-const CourseIdPage = ({params}:CourseIdParams) => {
+const CourseIdPage = async({params}:CourseIdParams) => {
+
+  const course=await db.course.findUnique({
+    where:{
+      id:params.courseId
+    },
+    include:{
+      chapters:{
+        where:{
+          isPublished:true
+        },
+        orderBy:{
+          position:'asc'
+        }
+      }
+    }
+  })
+
+  if(!course)return redirect('/');
+
+  return redirect(`/course/${course.id}/chapters/${course.chapters[0].id}`)
+
   return (
     <div>CourseAId {params.courseId}</div>
   )
