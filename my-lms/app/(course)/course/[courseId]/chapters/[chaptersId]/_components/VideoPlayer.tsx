79f5ce30b68bcfecbb2358488bdc6c810b24d57a
PALLAB GHOSH <gpallab405@gmail.com>
2024-01-21 11:33:34 +0530

    create <VideoPlayer/> component for individual chapter

---
+++ my-lms/app/(course)/course/[courseId]/chapters/[chaptersId]/_components/VideoPlayer.tsx
@@ -0,0 +1,63 @@
+"use client"
+
+import { cn } from "@/lib/utils"
+import MuxPlayer from "@mux/mux-player-react"
+import { Loader2, Lock } from "lucide-react"
+import { useState } from "react"
+
+ type VideoPlayerProps={
+    chaperId:string,
+    title:string
+    courseId:string
+    nextchapterId:string
+    playbackId:string
+    isLocked:boolean
+    completeOnEnd:boolean
+ }
+
+const VideoPlayer = ({ chaperId,
+    title,
+    courseId,
+    nextchapterId ,
+    playbackId ,
+    isLocked ,
+    completeOnEnd}:VideoPlayerProps) => {
+
+        const[isready,setready]=useState(false)
+  return (
+    <div className=" relative aspect-video">
+      {
+        !isready && !isLocked && (
+            <div className=" absolute inset-0 flex items-center justify-center bg-slate-800">
+               <Loader2 className=" h-8 w-8 animate-spin text-secondary"/>
+            </div>
+        )
+      }
+
+      {
+        isLocked && (
+            <div className=" absolute inset-0 flex items-center justify-center bg-slate-800 flex-col gap-y-2 text-secondary">
+              <Lock className=" h-8 w-8 "/>
+              <p className="text-small font-bold">
+                This chapter is locked
+              </p>
+            </div>
+        )
+      }
+
+      {
+        !isLocked && isready &&(
+            <MuxPlayer
+             title={title}
+             className={cn(!isready && "hidden")}
+             onCanPlay={()=>setready(true)}
+             onEnded={()=>{}}
+             autoPlay={playbackId}
+            />
+        )
+      }
+    </div>
+  )
+}
+
+export default VideoPlayer
