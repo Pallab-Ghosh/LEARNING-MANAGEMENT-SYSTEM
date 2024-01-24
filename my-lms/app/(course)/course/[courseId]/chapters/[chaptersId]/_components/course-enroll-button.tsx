5e5dc7eb9fe14e9d9147548a1059a439676bcc15
PALLAB GHOSH <gpallab405@gmail.com>
2024-01-24 23:56:32 +0530

    set a state variable and pass to button and call the stripe api for checkout

--- my-lms/app/(course)/course/[courseId]/chapters/[chaptersId]/_components/course-enroll-button.tsx
+++ my-lms/app/(course)/course/[courseId]/chapters/[chaptersId]/_components/course-enroll-button.tsx
@@ -1,7 +1,10 @@
 "use client"
 
 import { Button } from "@/components/ui/button"
+import axios from "axios"
 import { IndianRupee, ShoppingBag } from "lucide-react"
+import { useState } from "react"
+import toast from "react-hot-toast"
 
  type CourseEnrollButtonProps={
     price:number
@@ -9,10 +12,29 @@ import { IndianRupee, ShoppingBag } from "lucide-react"
  }
 
 const  CourseEnrollButton = ({price,courseId}:CourseEnrollButtonProps) => {
+  const[isloading,setloading]=useState(false)
+
+  const onclick=async()=>{
+     try 
+     {
+       setloading(true);
+       const response=await axios.post(`/api/course/${courseId}/checkout`)
+       window.location.assign(response.data.url);
+     } 
+      catch (error) {
+       toast.error('Something is wrong')
+     }
+     finally{
+      setloading(false)
+     }
+  }
+  
   return (
     <Button
     className=" w-full md:w-auto"
     size="sm"
+    onClick={onclick}
+    disabled={isloading}
     >
         
         <div>Enroll for {price}</div>
