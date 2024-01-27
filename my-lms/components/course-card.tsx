5cd0e27cf3c73ebc4fa1679093faf9324fbc6fd9
PALLAB GHOSH <gpallab405@gmail.com>
2024-01-27 20:14:10 +0530

    Add <CourseProgress/> component

--- my-lms/components/course-card.tsx
+++ my-lms/components/course-card.tsx
@@ -2,6 +2,7 @@ import Image from "next/image"
 import Link from "next/link"
 import { IconBadge } from "./icon-badge"
 import { BookOpen, IndianRupee } from "lucide-react"
+import CourseProgress from "./course-progress"
 
 type CourseCardProps={
     id:string,
@@ -41,16 +42,18 @@ const CourseCard = ({id,title,imageUrl,chaptersLength,price,progress,category}:C
 
                              <div className=" flex items-center gap-x-1 text-slate-500">
                                    <IconBadge size='sm' icon={BookOpen}/>
-                                    <span>
-                                    {chaptersLength}
-                                    {chaptersLength===1 ? ' Chapter' : ' Chapters'}
+                                    <span className=' text-blue-500'>
+                                    {chaptersLength} {chaptersLength===1 ? ' Chapter' : ' Chapters'}
                                     </span>
                              </div>
                         </div>
                          {
                             progress!==null ?(
                                 <div>
-                                    todo
+                                     <CourseProgress
+                                      value={progress}
+                                      variant={progress ===100 ? "success" :'default'}
+                                     />
                                 </div>
                             ):(
                                 <p className=" text-md md:text-sm font-medium flex">
