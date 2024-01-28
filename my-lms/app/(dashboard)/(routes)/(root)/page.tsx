6cb207a0feb0bc155ed8b958fd3a64d533bf5ae4
PALLAB GHOSH <gpallab405@gmail.com>
2024-01-28 23:11:18 +0530

    Move this page to root folder

---
+++ my-lms/app/(dashboard)/(routes)/(root)/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { UserButton } from "@clerk/nextjs";
+
+
+export default function Routepage() {
+  return (
+    <div>
+       <h1>dashboard page</h1>
+    </div>
+  )
+}
