e7ef3890d8740c593f201c2b792a55e1ed296121
PALLAB GHOSH <gpallab405@gmail.com>
2024-01-29 20:32:23 +0530

    check that course percentage in equivalent to 100 or not

--- my-lms/action/get-dashboard-courses.ts
+++ my-lms/action/get-dashboard-courses.ts
@@ -44,10 +44,12 @@ export const getDashboardCourses=async(userId:string):Promise<DashboardCoursesPr
             const progress=await getProgress({userId,courseId:course.id});
             course["progress"]=progress;
          }
+        // console.log('courses after add progress in getDashboardCourses',courses)
 
-         const completedCourses=courses.filter((course_value)=>course_value.progress===1000)
+         const completedCourses=courses.filter((course_value)=>course_value.progress===100)
          const coursesInProgress=courses.filter((course_value)=>(course_value.progress ?? 0) < 100)
          //course_value.progress less than 100 or null then 0.
+         
 
          return {
             completedCourses,
