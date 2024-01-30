de2ea1e68c896059ac22d84a503ad646dce54e40
PALLAB GHOSH <gpallab405@gmail.com>
2024-01-30 10:09:46 +0530

    Add the UI for completed courses ans courses in progress,integrate the  <InfoCard/> component and <CourseList/> component for show all courses purchase by user

--- my-lms/app/(dashboard)/(routes)/(root)/page.tsx
+++ my-lms/app/(dashboard)/(routes)/(root)/page.tsx
@@ -1,11 +1,41 @@
 import React from 'react'
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, auth } from "@clerk/nextjs";
+import { redirect } from 'next/navigation';
+import { getDashboardCourses } from '@/action/get-dashboard-courses';
+import CoursesList from '@/components/courses-list';
+import { CheckCircle, Clock } from 'lucide-react';
+import InfoCard from './components/info-card';
 
 
-export default function Routepage() {
+export default async function Dashboard() {
+
+  const {userId}=auth();
+
+  if(!userId)return redirect('/');
+
+  const {completedCourses,coursesInProgress}=await getDashboardCourses(userId)
+
   return (
-    <div>
-       <h1>dashboard page</h1>
+    <div className='p-6 space-y-4 '>
+         <div className=' grid grid-cols-1 sm:grid-cols-2 gap-4'>
+              <InfoCard
+              icon={Clock}
+              label="In Progress"
+              numberOfItems={coursesInProgress.length}
+              
+              /> 
+
+              <InfoCard
+              icon={CheckCircle}
+              label="Completed"
+              numberOfItems={completedCourses.length}
+              variant='success'
+              /> 
+          </div>  
+
+            <CoursesList
+             items={[...coursesInProgress,...completedCourses]}
+            />
     </div>
   )
 }
