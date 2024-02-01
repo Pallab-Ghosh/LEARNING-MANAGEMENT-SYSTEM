c7142dbba315cbc8577213e52207042c1ec5a381
PALLAB GHOSH <gpallab405@gmail.com>
2024-02-01 13:36:00 +0530

    check also here user is admin or not

--- my-lms/app/api/course/[courseid]/chapters/route.ts
+++ my-lms/app/api/course/[courseid]/chapters/route.ts
@@ -1,4 +1,5 @@
 import { db } from "@/lib/db";
+import { isTeacher } from "@/lib/teacher";
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 import { type } from "os";
@@ -16,7 +17,7 @@ export async function POST(req:Request,{params}:params_type)
         const {userId}=auth()
         const {title}=await req.json()
 
-        if(!userId)
+        if(!userId || isTeacher(userId))
         {
             return new NextResponse('Unauthorized',{status:401})
         }
