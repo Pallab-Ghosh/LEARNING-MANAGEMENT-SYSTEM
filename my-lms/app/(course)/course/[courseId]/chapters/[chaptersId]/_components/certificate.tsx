c300be49bdde22e0ee183ca35f4221d9ee5eaa0f
PALLAB GHOSH <gpallab405@gmail.com>
2024-02-15 21:44:30 +0530

    Update CSS of Certificate component

--- my-lms/app/(course)/course/[courseId]/chapters/[chaptersId]/_components/certificate.tsx
+++ my-lms/app/(course)/course/[courseId]/chapters/[chaptersId]/_components/certificate.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react';
 
 
@@ -7,19 +8,38 @@ type CertificateProps={
 }
 const Certificate = ({ username, courseName }:CertificateProps) => {
   return (
-    <div style={{ textAlign: 'center', padding: '20px', border: '1px solid #ccc', borderRadius: '8px' }}>
-      <h2>Certificate of Completion</h2>
-      <p>This is to certify that</p>
-      <h3>{username}</h3>
-      <p>has successfully completed the course</p>
-      <h3>{courseName}</h3>
+    <div className= 'bg-slate-50 w-full flex flex-col m-2  border-double border-x-4 border-y-4 border-teal-900'>
 
-      <div style={{ marginTop: '20px' }}>
-        <img
-          src="https://your-logo-url.com/logo.png" // Replace with your logo URL http://www.w3.org/2000/svg
+         <Image
+          src="/logo.svg" 
           alt="Logo"
-          style={{ maxWidth: '150px', maxHeight: '100px' }}
+          width={50}
+          height={20}
         />
+        <div className=' flex flex-col justify-center mt-8'>
+             <h1 className=' text-center text-teal-500 font-extrabold -m-2 text-lg'>Skill</h1>
+             <h1 className=' text-center text-blue-900 font-extrabold text-lg'>Learning</h1>
+        </div>
+
+        <div className=' p-8'>
+            <h1 className=' pt-6 font-bold  text-gray-700 text-center justify-center'>
+              Certificate of Completion
+              </h1>
+        </div>
+
+         <div className='pt-3 whitespace-normal flex justify-center'>
+              <h3 className='text-gray-700 '>
+              This is to certify that {username} has successfully completed the course <br/> <span className=' font-extrabold text-rose-900'>{courseName}</span></h3>
+         </div>
+
+         <div className=' flex flex-col justify-center font-bold'>
+             <h1 className=' text-center  text-gray-700'>Provided By</h1>
+             <h1 className=' text-center  text-gray-700'>Skill Learning Academy</h1>
+        </div>
+      
+
+      <div className=' mt-20' style={{ marginTop: '20px' }}>
+        
       </div>
     </div>
   );
