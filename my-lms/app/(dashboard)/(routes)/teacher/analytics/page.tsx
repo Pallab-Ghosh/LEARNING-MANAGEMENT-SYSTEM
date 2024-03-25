b7c0ba0dbf2bc67e0b203abc47b16208336c76bf
PALLAB GHOSH <gpallab405@gmail.com>
2024-03-25 19:23:24 +0530

    Fetch the data from getAnalytics function and render them in   <DataCard/>

--- my-lms/app/(dashboard)/(routes)/teacher/analytics/page.tsx
+++ my-lms/app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import { getAnalytics } from '@/action/get-analytics';
+import { auth } from '@clerk/nextjs'
+import { redirect } from 'next/navigation';
+ 
+import DataCard from './_component/data-card';
+import Chart from './_component/chart';
 
-const Analytics = () => {
+
+
+
+const Analytics = async () => {
+
+  const {userId,user}=auth();
+ 
+
+  if(!userId)return redirect('/');
+  console.log('userId',userId)
+
+  const {data,totalrevenue,totalSales}=await getAnalytics(userId);
+ 
+
+   
   return (
-    <div>Analytics page</div>
+    <div className=' p-6 '>
+       <div className=' grid grid-cols-1 md:grid-cols-2 gap-4 mb-4'>
+           <DataCard
+            label='Total Reveneu'
+            value={totalrevenue!}
+            id="reveneu"
+           />
+
+            <DataCard
+            label='Total Sales'
+            value={totalSales!}
+            id="sales"
+           />
+       </div>
+       <Chart
+        data={data}
+       />
+      
+    </div>
   )
 }
 
