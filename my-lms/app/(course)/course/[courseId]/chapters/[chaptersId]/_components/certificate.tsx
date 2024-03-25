a660f67836902466481204ad62d3087253f1007d
PALLAB GHOSH <gpallab405@gmail.com>
2024-03-25 19:58:34 +0530

    Adjust Css for mobile responsive

--- my-lms/app/(course)/course/[courseId]/chapters/[chaptersId]/_components/certificate.tsx
+++ my-lms/app/(course)/course/[courseId]/chapters/[chaptersId]/_components/certificate.tsx
@@ -10,10 +10,10 @@ type CertificateProps={
 }
 const Certificate = ({ username, courseName }:CertificateProps) => {
   return (
-    <div className= 'bg-slate-50 lg:w-full lg:h-full h-full w-96 p-5 flex flex-col m-2  border-double border-x-4 border-y-4 border-teal-900 relative'>
+    <div className= 'bg-slate-50 lg:w-full lg:h-full h-96 w-96 p-5 flex flex-col   m-auto  border-double border-x-4 border-y-4 border-teal-900 relative'>
 
-        <div className=' mt-0'>
-           <Image src="/logo.svg" alt="Logo"  width={50}  height={20}/>
+        <div className=' absolute mt-0'>
+           <Image src="/logo.svg" alt="Logo"  width={70}  height={40}/>
         </div>
 
         <div className='flex flex-col justify-center mt-1'>
@@ -21,15 +21,15 @@ const Certificate = ({ username, courseName }:CertificateProps) => {
              <h1 className=' text-center text-blue-900 font-extrabold text-lg'>Learning</h1>
         </div>
 
-        <div className='mt-1'>
-            <h1 className=' pt-6 font-bold  text-gray-700 text-center justify-center'>
+        <div className='mt-2'>
+             <p className='font-bold  text-gray-700 text-center justify-center'>
               Certificate of Completion
-              </h1>
+              </p>
         </div>
 
-         <div className='pt-3 whitespace-normal flex justify-center'>
+         <div className='mt-4 whitespace-normal flex justify-center'>
               <h3 className='text-gray-700 '>
-              This is to certify that {username} has successfully completed the online course <br/> <span className=' font-extrabold text-rose-900'>{courseName}</span></h3>
+              This is to certify that {username} has successfully completed the course <br/> <span className=' font-extrabold text-rose-900'>{courseName}</span></h3>
          </div>
 
          <div className=' flex flex-col justify-center font-bold pt-5'>
@@ -38,7 +38,7 @@ const Certificate = ({ username, courseName }:CertificateProps) => {
         </div>
       
 
-      <div className=' pt-9 pb-2  flex justify-end' >
+      <div className='mt-3 pb-2  flex justify-end' >
              <h3 className='underline decoration-slate-900 mr-2'>Pallab Ghosh</h3>
       </div>
     </div>
