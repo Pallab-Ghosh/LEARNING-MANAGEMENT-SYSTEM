f13575613ee1c7b41480c9237794383d5ad617a9
PALLAB GHOSH <gpallab405@gmail.com>
2024-03-25 19:21:08 +0530

    Install the packages for html to pdf and pdf creation

--- my-lms/app/(course)/course/[courseId]/chapters/[chaptersId]/_components/course-certificate.tsx
+++ my-lms/app/(course)/course/[courseId]/chapters/[chaptersId]/_components/course-certificate.tsx
@@ -1,76 +1,84 @@
 "use client"
-
-import { Button } from '@/components/ui/button';
-import { useConfettiStore } from '@/hooks/use-confetti-store';
-import axios from 'axios';
-import { CheckCircle, XCircle } from 'lucide-react';
 import { useRouter } from 'next/navigation';
-import { jsPDF } from 'jspdf';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import toast from 'react-hot-toast';
 import Certificate from './certificate';
-import { auth, useAuth } from '@clerk/nextjs';
+import { useAuth } from '@clerk/nextjs';
 import Link from 'next/link';
 import { cn } from '@/lib/utils';
- 
-type  CourseCertificateProps={
-    chapterId:string,
-    courseId:string,
-    nextChapterId :string,
-    isCompleted:boolean
-    courseName:string
+import { jsPDF } from 'jspdf'; 
+import html2canvas from 'html2canvas';
+import { Loader } from 'lucide-react';
+
+type CourseCertificateProps = {
+    chapterId: string,
+    courseId: string,
+    nextChapterId: string,
+    isCompleted: boolean,
+    courseName: string
 }
-const CourseCertificate = ({chapterId,courseId,nextChapterId,isCompleted,courseName}: CourseCertificateProps) => {
-  
-   // const Icon=isCompleted ? XCircle : CheckCircle
 
-    const router=useRouter()
-    const[isloading,setloading]=useState(false)
-    const {userId}=useAuth();
-    const [showCertificate, setShowCertificate] = useState(false);
+const CourseCertificate = ({ chapterId, courseId, nextChapterId, isCompleted, courseName }: CourseCertificateProps) => {
 
- 
 
-    const handleDownloadCertificate = () => {
+    const router = useRouter();
+    const [isloading, setloading] = useState(false);
+    const { userId } = useAuth();
+    const licenseCertficateProp = useRef<HTMLDivElement>(null);
 
-      setShowCertificate(true)
-        const certificateElement = document.getElementById('certificate');
-      
-        if (certificateElement) {
-          const doc = new jsPDF();
-          doc.html(certificateElement, {
-            callback: function () {
-              doc.save('certificate.pdf');
-            },
-          });
+
+
+    const handleDownloadCertificate = async () => {
+
+
+        const inputData = licenseCertficateProp.current;
+        if (inputData) {
+    
+            const canvas = await html2canvas(inputData);
+            const imgdata = canvas.toDataURL("image/png");
+
+            try {
+                  const pdf = new jsPDF({
+                      orientation: 'landscape',
+                      unit: 'px',
+                      format: "a4"
+                  });
+
+                  const width = pdf.internal.pageSize.getWidth();
+                  const height = (canvas.height * width) / canvas.width;
+
+                  pdf.addImage(imgdata, "PNG", 0, 0, width, height);
+                  pdf.save("License.pdf");
+            } 
+            catch (error) {
+                console.log(error);
+            }
+        } 
+
+        else 
+        {
+            console.log("Certificate element is not available.");
         }
-      };
-
+    };
 
     return (
-      <div className=' h-full w-full bg-neutral-200'>
-         {
-         showCertificate && (
-            <div id="certificate" className={cn(!isCompleted && 'hidden',' h-96 flex items-center w-full')}>
-                    <Certificate 
-                    username={userId!} 
-                    courseName={courseName}
-                    
-                    />
-            </div>
-          )}
+        <div className='h-full w-full bg-neutral-200'>
+           {
+             <div ref={licenseCertficateProp} id="certificate" className={cn(!isCompleted && 'hidden', 'h-full flex items-center w-full')}>
+                                <Certificate 
+                                    username={userId!} 
+                                    courseName={courseName}
+                                />
+                        </div>
+           }
 
-         
-          
-            <div className=' flex border-double border-y-pink-950 items-center justify-center  h-10 w-full text-center'>
-              <Link href="#" role="button" onClick={handleDownloadCertificate} className="text-lg font-bold">
-                 Download Certificate
-              </Link>
-           </div>
-     
-         
-      </div>
-  )
+            <div className='flex border-double border-y-pink-950 items-center justify-center h-12 w-full text-center'>
+                <Link href="#" role="button" onClick={handleDownloadCertificate} className="text-lg font-bold">
+                    Download Certificate
+                </Link>
+            </div>
+        </div>
+    );
 }
 
-export default CourseCertificate
+export default CourseCertificate;
