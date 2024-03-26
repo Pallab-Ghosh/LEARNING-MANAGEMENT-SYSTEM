91b30ab1db9bbf561a5653f0a4fa856461d02aaf
PALLAB GHOSH <gpallab405@gmail.com>
2024-03-26 20:52:55 +0530

    Install module for UUID and pass it to server for updating user with course and unique id

--- my-lms/app/(course)/course/[courseId]/chapters/[chaptersId]/_components/course-certificate.tsx
+++ my-lms/app/(course)/course/[courseId]/chapters/[chaptersId]/_components/course-certificate.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { useRouter } from 'next/navigation';
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import toast from 'react-hot-toast';
 import Certificate from './certificate';
 import { useAuth } from '@clerk/nextjs';
@@ -9,73 +9,104 @@ import { cn } from '@/lib/utils';
 import { jsPDF } from 'jspdf'; 
 import html2canvas from 'html2canvas';
 import { Loader } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import axios from 'axios';
+import { string } from 'zod';
+const { v4: uuidv4 } = require('uuid');
+
 
 type CourseCertificateProps = {
     chapterId: string,
     courseId: string,
     nextChapterId: string,
     isCompleted: boolean,
-    courseName: string
+    courseName: string,
+    current_user : string
 }
 
-const CourseCertificate = ({ chapterId, courseId, nextChapterId, isCompleted, courseName }: CourseCertificateProps) => {
+const CourseCertificate = ({ chapterId, courseId, nextChapterId, isCompleted, courseName , current_user }: CourseCertificateProps) => {
 
 
     const router = useRouter();
     const [isloading, setloading] = useState(false);
     const { userId } = useAuth();
     const licenseCertficateProp = useRef<HTMLDivElement>(null);
-
-
+    const [isReady , setReady]=useState(false)
+    const [user_credential_Id , set_user_credential_Id]=useState('')
+    
+    
+    
 
     const handleDownloadCertificate = async () => {
 
+        let credential_Id :string = uuidv4();
+        set_user_credential_Id(credential_Id);
 
-        const inputData = licenseCertficateProp.current;
-        if (inputData) {
-    
-            const canvas = await html2canvas(inputData);
-            const imgdata = canvas.toDataURL("image/png");
+        setloading(true)
 
-            try {
-                  const pdf = new jsPDF({
-                      orientation: 'landscape',
-                      unit: 'px',
-                      format: "a4"
-                  });
+           const certificate = await axios.post(`/api/course/certificate`,{
+              courseId:courseId,
+              credential_Id : credential_Id
+           })
 
-                  const width = pdf.internal.pageSize.getWidth();
-                  const height = (canvas.height * width) / canvas.width;
+         console.log("certificate object" , certificate)
+           if(certificate.data.credentialId!='')
+           {
+                    const inputData = licenseCertficateProp.current;
 
-                  pdf.addImage(imgdata, "PNG", 0, 0, width, height);
-                  pdf.save("License.pdf");
-            } 
-            catch (error) {
-                console.log(error);
-            }
-        } 
-
-        else 
-        {
-            console.log("Certificate element is not available.");
-        }
+                    if (inputData)
+                     {
+                
+                        const canvas = await html2canvas(inputData);
+                        const imgdata = canvas.toDataURL("image/png");
+            
+                        try {
+                            const pdf = new jsPDF({
+                                orientation: 'landscape',
+                                unit: 'px',
+                                format: "a4"
+                            });
+            
+                            const width = pdf.internal.pageSize.getWidth();
+                            const height = (canvas.height * width) / canvas.width;
+            
+                            pdf.addImage(imgdata, "PNG", 0, 0, width, height);
+                            pdf.save("License.pdf");
+                        } 
+                        catch (error) {
+                            console.log(error);
+                        }
+                        finally{
+                            setReady(true)
+                            setloading(false)
+                        }
+                    } 
+            
+                    else 
+                    {
+                        console.log("Certificate element is not available.");
+                    }
+           }
     };
 
     return (
-        <div className='h-full w-full bg-neutral-200'>
+        <div className='h-full w-full bg-slate-50'>
            {
              <div ref={licenseCertficateProp} id="certificate" className={cn(!isCompleted && 'hidden', 'h-full flex items-center w-full')}>
-                                <Certificate 
-                                    username={userId!} 
-                                    courseName={courseName}
-                                />
+                              {
+                                isReady &&  <Certificate 
+                                                username={current_user!} 
+                                                courseName={courseName}
+                                                user_credential_Id = {user_credential_Id!}
+                                            />
+                              }
                         </div>
            }
 
             <div className='flex border-double border-y-pink-950 items-center justify-center h-12 w-full text-center'>
-                <Link href="#" role="button" onClick={handleDownloadCertificate} className="text-lg font-bold">
+                <Button disabled={isloading}  onClick={handleDownloadCertificate} className="text-lg font-bold">
                     Download Certificate
-                </Link>
+                </Button>
             </div>
         </div>
     );
